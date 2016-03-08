b762709054709918dc1666a16205c8d6de307f7b
DerekScheller <derekscheller23@gmai.com>
2016-03-08 14:26:31 -0600

    Functional dice roller tested and ready for use.

---
+++ src/Dice.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Dice {
+	int diceOutCome;
+	int numberOfSides;
+	Random rand = new Random();
+	public Dice(){
+		
+	}
+		public int RollDice(int NumberOfSides){
+			numberOfSides = NumberOfSides;
+			diceOutCome = rand.nextInt(NumberOfSides) + 1;
+			return diceOutCome;
+		}
+		
+}
+
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Dice Game Specs
+		 Game must be more than just war with dice
+		 Game must use OOP programming (classes and instantiation)
+	 Game must use at least one of each dice (d4, d6, d8, d10, d12, d20)
+		 Game must include options single player (against the AI)
+		 Game must include multiplayer
+		 Only class instantiation, variable declaration and assignment, and method calling is permitted in the main class/function
+		You make the rules for your game, but it must be dice based and follow the above rules.*/
+		Dice diceRoll = new Dice();
 
+	
 	}
 
 }
