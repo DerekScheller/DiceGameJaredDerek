02e117f478fc899ce7972410e6d0be04f44aa11a
DerekScheller <derekscheller23@gmai.com>
2016-03-09 17:05:30 -0600

    updating to newest version

--- src/DifficultySelection.java
+++ src/DifficultySelection.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.List;
@@ -6,12 +7,49 @@ public class DifficultySelection {
 	boolean answercorrect = true;
 	int selecteddice=0;
 	Scanner scanner = new Scanner(System.in);
+	Dice rndSelectAI = new Dice();
 	String dicepickedbyplayer;
 	boolean answertrueorfalse;
 	public DifficultySelection()
 	{
 	}
-	public int PickYourDice()
+	public int PickYourDiceAI()
+	{
+		int AIDiffSelection = rndSelectAI.RollDice(6);
+		switch(AIDiffSelection){
+		case 0:
+		{
+			selecteddice=4;
+			return selecteddice;
+		}
+		case 1:
+		{
+			selecteddice=6;
+			return selecteddice;
+		}
+		case 2:
+		{
+			selecteddice=8;
+			return selecteddice;
+		}
+		case 3:
+		{
+			selecteddice=10;
+			return selecteddice;
+		}
+		case 4:
+		{
+			selecteddice=12;
+			return selecteddice;
+		}
+		default:
+		{
+			selecteddice=20;
+			return selecteddice;
+		}
+		}
+	}
+	public int PickYourDiceHuman()
 	{
 		List<String> validplayerselection= Arrays.asList("4","6","8","10","12","20");
 		System.out.println("Which difficulty dice would you like? (4,6,8,10,12,20) ");
@@ -53,6 +91,7 @@ public class DifficultySelection {
 		}
 		}
 	}
+	
 	public boolean difficultyselection() 
 	{
 		Questions questions = new Questions();
