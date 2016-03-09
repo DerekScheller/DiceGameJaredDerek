636f7bfbe8dd3dbd9019fcfaa44418ff83df9569
DerekScheller <derekscheller23@gmai.com>
2016-03-08 19:27:04 -0600

    I finished the game set up formula and have the playing style i.e. turn generator complete, at this point a function to ask a question needs to be imput into the turn generator.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,5 +1,5 @@
 import java.util.List;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GameBoard {
@@ -7,6 +7,7 @@ public class GameBoard {
 	Dice diceRoll = new Dice();
 	Scanner scanner = new Scanner(System.in);
 	List<Player> players = new ArrayList<>();
+	boolean answer;
 
 	public GameBoard() {
 	}
@@ -17,31 +18,33 @@ public class GameBoard {
 		for(int i=0; i<totalOfPlayers; i++){
 			System.out.println("What is this players name?");
 			String inputName = scanner.next();
-			System.out.println("Is this player human or AI?");
+			System.out.println("Is this player human? 'Y' or 'N'");
 			String inputPlayerType = scanner.next();
-			players.add(new Player(inputName, inputPlayerType);
+			if(inputPlayerType.contains("Y")){
+				inputPlayerType = "Human";
+			}else{
+				inputPlayerType = "AI";
+			}
+			players.add(new Player(inputName, inputPlayerType));
 		}
-		public void FullTurn(List<Player> PlayerNames){
-			PlayerNames.add(new Player("john"));
-			Player.get.(NumOfPlayers)score = 0;
-			Player.type = Userinput;
-			PlayerNames.get(0).score = 7;
-		}
-			for(Player player : PlayerNames){
+		public void FullTurn(List<Player> players){
+				for(Player player : players){
 				System.out.println("It is " + player + "'s turn.");
 				System.out.println("Your current  score is: " + player.playerTotalScore);
-				int diceSelector = 	
-				int playerDiceRoll = diceRoll.RollDice(DiceSelector);
+				int diceSelector = 	20;
+				int playerDiceRoll = diceRoll.RollDice(diceSelector);
 				System.out.println("You rolled a: " + playerDiceRoll);
-				PlayerTotalScore
+				if (answer == true){
+					player.playerTotalScore = player.playerTotalScore + playerDiceRoll;
+				}
+				
 				}
 		}
 
-	public void YouWin(List<String> PlayerNames, List<Integer> PlayerTotalScore) {
-		for (int score : PlayerTotalScore) {
-			if (score >= 100) {
-				String PlayerName = PlayerNames.get();
-				System.out.println("You Win: ", +PlayerName);
+	public void YouWin(List<Player> players) {
+		for (Player player : players) {
+			if (player.playerTotalScore >= 100) {
+				System.out.println("You Win: " + player.name);
 			}
 
 		}
